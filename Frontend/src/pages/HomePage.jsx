3bbd9001bd2286c65e20bc20dbcb47cd467c2a38
umutbarandemir <59203891+umutbarandemir@users.noreply.github.com>
2025-06-04 02:03:05 +0300

    MovieCard, states

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+// 📁 src/pages/HomePage.jsx
+import { useEffect } from 'react';
+import useMovieStore from '../store/useMovieStore.js';
+import MovieCard from '../components/MovieCard.jsx';
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const { movies, fetchMovies, loading, error } = useMovieStore();
 
-export default HomePage
+  useEffect(() => {
+    fetchMovies();
+  }, [fetchMovies]);
+
+  return (
+    <div className="p-4 space-y-4">
+      <h1 className="text-3xl font-bold">Hot Movies</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p className="text-red-500">{error}</p>}
+      <div className="flex flex-wrap gap-6">
+        {movies.map((movie) => (
+          <MovieCard key={movie._id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
